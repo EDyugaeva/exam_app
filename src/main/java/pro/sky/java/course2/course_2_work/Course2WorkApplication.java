4735a6859c7f04a4fb567f92ebb73ddb61e31637
EDyugaeva <ed2408@yandex.ru>
2022-03-08 21:33:43 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/course_2_work/Course2WorkApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.course_2_work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course2WorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Course2WorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course2/course_2_work/Course2WorkApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.course_2_work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course2WorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
